dbb66168845c1d942546427f27b95f76a2ce0545
Firas <firasaldabbas+git@gmail.com>
2019-07-16 21:22:36 -0400

    Snake can no longer double back on itself. Snake's speed is now an init variable

--- classes/snake.py
+++ classes/snake.py
@@ -3,10 +3,10 @@ from constants.enums import Directions
 
 
 class Snake:
-    def __init__(self, start_length=settings.START_LENGTH, start_location=(0, 0), start_direction=Directions.RIGHT):
+    def __init__(self, speed=10, start_length=settings.START_LENGTH, start_location=(0, 0), start_direction=Directions.RIGHT):
         self.x = start_location[0]
         self.y = start_location[1]
-        self.speed = 1
+        self.speed = speed
         self.direction = start_direction
         self.controls = {} # Different snakes will have different controls
 
@@ -23,7 +23,16 @@ class Snake:
         self.y += self.speed * direction[1]
     
     def set_direction(self, direction):
-        self.direction = direction
+        # No doubling back on yourself, foo'
+        not_allowed = {
+            Directions.RIGHT: Directions.LEFT,
+            Directions.LEFT: Directions.RIGHT,
+            Directions.UP: Directions.DOWN,
+            Directions.DOWN: Directions.UP
+        }
+
+        if not not_allowed[direction] == self.direction:
+            self.direction = direction
 
     # def move_right(self):
     #     self.x += self.speed
