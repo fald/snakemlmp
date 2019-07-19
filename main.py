de4d7a7fc40bdc7e6a34d55718a468d93ef7a203
Firas <firasaldabbas+git@gmail.com>
2019-07-18 22:33:09 -0400

    Modified todo list

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import pygame
 #   Game over when no more apples to eat
 #   Put collisions elsewhere than in application loop
 #   Game state handling (not just crashing on game over lol)
-#   Menus
+#   Menus - scoreboard, option settings
 #   Roll around walls
+#   AI control, AI options on-screen
 
 if __name__ == "__main__":
     window = App()
