7ca25410f3a11542aa621992c30ac9e45b445ab6
Firas <firasaldabbas+git@gmail.com>
2019-07-22 00:02:57 -0400

    Window class done.

--- classes/windows.py
+++ classes/windows.py
@@ -4,6 +4,7 @@ import pygame
 
 class Window:
     main_window_resolution = RESOLUTION
+    window_buffer = WINDOW_BUFFER
 
     def __init__(
         self, resolution=RESOLUTION, surface=None, color=BACKGROUND,
@@ -18,31 +19,12 @@ class Window:
 
     @property
     def abs_location(self):
-        # Fucking gross.
-        # TODO: Find more elegant way to do this :/
-        x, y = 0, 0
-        size_x, size_y = main_surface.get_size()    # main_window_resolution
-        sub_x, sub_y = sub_surface.get_size()       # self.resolution
-        location = self.rel_location                #
-        if location[0] == 0:
-            x_pos = WINDOW_BUFFER
-        elif location[0] == 0.5:
-            x_pos = int(size_x / 2) - int(sub_x / 2)
-        elif location[0] == 1:
-            x_pos = size_x - sub_x - WINDOW_BUFFER
-
-            # pattern:
-            #   pos = size*loc - sub*loc - window_buffer * (-1, 0, 1)
-            #
-            #
-            #
-            #
-
-        if location[1] == 0:
-            y_pos = WINDOW_BUFFER
-        elif location[1] == 0.5:
-            y_pos = int(size_y / 2) - int(sub_y / 2)
-        elif location[1] == 1:
-            y_pos = size_y - sub_y - WINDOW_BUFFER
-
-        return x_pos, y_pos
+        x, y = (
+            relative_location * (main_resolution - own_resolution + 2 * Window.window_buffer) - Window.window_buffer
+            for 
+            relative_location, main_resolution, own_resolution 
+            in 
+            zip(self.rel_location, Window.main_window_resolution, self.resolution)
+            # self.rel_location * (main_window_resolution - self.resolution + 2 * window_buffer) - window_buffer
+        )
+        return x, y
