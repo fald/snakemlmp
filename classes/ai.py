507a93e7a4b49534afa3cc41f0b09d2bcc3cf7af
Firas <firasaldabbas+git@gmail.com>
2019-07-29 19:26:42 -0400

    start on formulae for ai

--- classes/ai.py
+++ classes/ai.py
@@ -27,4 +27,35 @@ class Basic(AI):
         return choice(AI.outputs)
 
 class ML(AI):
-    pass
+    def __init__(self):
+        super(ML, self).__init__()
+    
+    def determine_move(self, board):
+        pass
+
+    def convert_relative_to_absolute(self, relative):
+        # Relative left
+        # If facing abs UP -> LT        (0, -1) ->  (-1, 0)     Flip
+        #               DN -> RT        (0, 1)  ->  (1, 0)      Flip
+        #               RT -> UP        (1, 0)  ->  (0, -1)     Flip + Invert
+        #               LT -> DN        (-1, 0) ->  (0, 1)      Flip + Invert
+        # Relative right
+        # If facing abs UP -> RT        (0, -1) ->  (1, 0)      Flip + Invert
+        #               RT -> DN        (1, 0)  ->  (0, 1)      Flip
+        #               DN -> LT        (0, 1)  ->  (-1, 0)     Flip + Invert
+        #               LT -> UP        (-1, 0) ->  (0, -1)     Flip
+        # Relative ahead
+        # No change
+
+        # If not ahead,
+        # a, b = b, a
+        #   if relative left and b == 0:
+        #       a *= -1
+        #   if relative left and b != 0:
+        #       b *= -1
+    
+    def check_obstacle(self, direction):
+        pass
+    
+    def check_obstacles(self):
+        pass
