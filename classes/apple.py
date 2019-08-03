abe1266e52ff91c1368ed23343af70fd35454344
Firas <firasaldabbas+git@gmail.com>
2019-08-03 15:02:51 -0400

    apple updated

--- classes/apple.py
+++ classes/apple.py
@@ -6,17 +6,26 @@ from constants import settings
 class Apple(Block):
     def __init__(self, image, location=None, visible=True, parent=None):
         super(Apple, self).__init__(location, image, visible, parent)
-        self.location = self.new_location(self.parent)
+        self.new_location()
 
     def new_location(self):
+        # candidate_locations = []
+        # for i in range(self.parent.dimensions[0]):
+        #     for j in range(self.parent.dimensions[1]):
+        #         candidate_locations.append((i, j))
+        # for snake in self.parent.components['snakes']:
+        #     for segment in snake.body:
+        #         if segment.grid_location in candidate_locations: # Just for while the snake can go out of bounds
+        #             candidate_locations.remove(segment.grid_location)
+        #     candidate_locations.remove(snake.grid_location)
+        # self.grid_x, self.grid_y = choice(candidate_locations)
+
+        occupied = self.parent.is_occupied()
         candidate_locations = []
-        for i in range(self.parent.dimensions[0]):
-            for j in range(self.parent.dimensions[1]):
-                candidate_locations.append((i, j))
-        for snake in self.parent.components['snakes']:
-            for segment in snake.body:
-                if segment.grid_location in candidate_locations: # Just for while the snake can go out of bounds
-                    candidate_locations.remove(segment.grid_location)
-            candidate_locations.remove(snake.grid_location)
+        for i in len(occupied[0]):
+            for j in len(occupied[1]):
+                if occupied[i][j]:
+                    candidate_locations.append((i, j))
         self.grid_x, self.grid_y = choice(candidate_locations)
+
 
